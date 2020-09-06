374760a2090ce118a244752b1a00f07bb2bbb447
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-09-06 23:52:18 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -4,9 +4,11 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
+  "SELECT MIN(gpa)"
 end
 
 def average_student_gpa
+  "SELECT AVG(gpa) FROM students"
 end
 
 def total_tardies_for_all_students
