b31a48ccc250b9e6aa7a7820a7fe7a38d86aae92
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-09-06 23:50:54 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -10,8 +10,9 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
+  "SUM"
 end
 
 def average_gpa_for_9th_grade
-  "SELECT AVG(gpa) FROM students WHERE grade=9;"
+  "SELECT AVG(gpa) FROM students WHERE grade = 9"
 end
